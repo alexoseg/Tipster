f5556e058d09fe92a12d9da847b0195b9e9db65c
Alex Oseguera <aoseg@stanford.edu>
2020-06-23 13:09:24 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Alex Oseguera on 6/23/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
